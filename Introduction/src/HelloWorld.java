2a004a135cd1da98be0a12129e5453231584ef14
aktunc-STMU <aktunc@gmail.com>
2015-01-25 19:40:21 -0600

    This is a test commit.

---
+++ Introduction/src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int a;
+		a = 5;
+		
+		double c = 4.5;
+		
+		float d = 6.7f;
+		
+		boolean t = false;
+		
+		char z = 's';
+		
+		long v = 35551413155513131L;
+	}
+
+}
